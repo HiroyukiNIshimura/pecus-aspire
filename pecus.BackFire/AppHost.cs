02664aa8ac3d71f9e0e2760fd299c3256b447056
Nishimura Hiroyuki <nishimura@bright-l.co.jp>
2025-10-24 16:50:55 +0900

    不要コードを整理

--- pecus.BackFire/AppHost.cs
+++ pecus.BackFire/AppHost.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 builder.AddRedisClient("redis");
 
+//ここでは何もしないHangfireクライアントとジョブを実行するサーバーを登録する
 builder.Services.AddHangfire((serviceProvider, config) =>
 {
     var redis = serviceProvider.GetRequiredService<IConnectionMultiplexer>();
