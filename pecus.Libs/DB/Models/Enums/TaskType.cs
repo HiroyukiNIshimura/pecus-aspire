0b2e67abeae8123918a67eb7c82a78923e10b644
nishimura <nishimura@bright-l.co.jp>
2025-11-01 22:02:20 +0900

    feat: タスクの種類に「打ち合わせ」、「商談」、「要件確認」を追加

--- pecus.Libs/DB/Models/Enums/TaskType.cs
+++ pecus.Libs/DB/Models/Enums/TaskType.cs
@@ -41,6 +41,21 @@ public enum TaskType
     Research = 7,
 
     /// <summary>
+    /// 打ち合わせ
+    /// </summary>
+    Meeting = 8,
+
+    /// <summary>
+    /// 商談
+    /// </summary>
+    BusinessNegotiation = 9,
+
+    /// <summary>
+    /// 要件確認
+    /// </summary>
+    RequirementsConfirmation = 10,
+
+    /// <summary>
     /// その他
     /// </summary>
     Other = 99,
