553803894230679843784bc6238cdf328a9a8215
Nishimura Hiroyuki <nishimura@bright-l.co.jp>
2025-11-17 17:17:26 +0900

    feat(migrations): update WorkspaceItem schema to make DueDate nullable and remove Content column
    - Removed the Content column from the WorkspaceItems table.
    - Altered the DueDate column to be nullable in the WorkspaceItems table.

---
+++ pecus.DbManager/Migrations/20251117072942_UpdateWorkspaceItem_MakeDueDateNullableAndRemoveContent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pecus.DbManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWorkspaceItem_MakeDueDateNullableAndRemoveContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "WorkspaceItems");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "WorkspaceItems",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DueDate",
+                table: "WorkspaceItems",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "WorkspaceItems",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
