dcd9d87901f6901d5a042fd2cc5c70fb1b079705
j69 <nishimura@bright-l.co.jp>
2025-11-26 09:27:11 +0900

    Revert "fix: ImagesPluginの引数を修正し、型を明示化"
    This reverts commit 1f1e126da762af8708ed155360fba5aa83976c20.

--- packages/notion-like-editor/src/plugins/ImagesPlugin/index.tsx
+++ packages/notion-like-editor/src/plugins/ImagesPlugin/index.tsx
@@ -198,7 +198,7 @@ export function InsertImageDialog({
 	);
 }
 
-export default function ImagesPlugin(): JSX.Element | null {
+export default function ImagesPlugin({ captionsEnabled }: { captionsEnabled?: boolean }): JSX.Element | null {
 	const [editor] = useLexicalComposerContext();
 
 	useEffect(() => {
@@ -374,7 +374,7 @@ function canDropImage(event: DragEvent): boolean {
 }
 
 function getDragSelection(event: DragEvent): Range | null | undefined {
-	let range: Range | null | undefined;
+	let range;
 	const domSelection = getDOMSelectionFromTarget(event.target);
 	if (document.caretRangeFromPoint) {
 		range = document.caretRangeFromPoint(event.clientX, event.clientY);
