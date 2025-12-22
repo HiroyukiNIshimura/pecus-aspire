865352ba7ce4905b9a37905a0aefbf76acce3021
Nishimura Hiroyuki <nishimura@bright-l.co.jp>
2025-12-22 16:08:26 +0900

    feat: Botのアクティベーション条件を変更し、特定の条件での応答を改善

--- pecus.Libs/Hangfire/Tasks/Bot/GroupChatReplyTask.cs
+++ pecus.Libs/Hangfire/Tasks/Bot/GroupChatReplyTask.cs
@@ -162,7 +162,7 @@ public class GroupChatReplyTask : GroupChatReplyTaskBase
     /// <param name="senderUserId">メッセージを送信したユーザーのID</param>
     public async Task SendReplyAsync(int organizationId, int roomId, int triggerMessageId, int senderUserId)
     {
-        if (!BotTaskUtils.ShouldActivateBot(130))
+        if (!BotTaskUtils.ShouldActivateBot(33))
         {
             return;
         }
