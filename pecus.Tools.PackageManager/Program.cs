3e0bc6bcbb8ab1c333c6a9b9b7dfe407e86579eb
j69 <nishimura@bright-l.co.jp>
2026-02-25 10:31:58 +0900

    エージェントに自主的に作成させたが、結果dotnet outdatedをそのまま使った方がいいので廃止

--- pecus.Tools.PackageManager/Program.cs
+++
@@ -1,9 +0,0 @@
-using Pecus.Tools.PackageManager.Commands;
-using System.CommandLine;
-
-var rootCommand = new RootCommand("Pecus パッケージマネージャー - NuGet パッケージの依存関係管理ツール");
-
-rootCommand.AddCommand(new CheckCommand());
-rootCommand.AddCommand(new UpdateCommand());
-
-return await rootCommand.InvokeAsync(args);
